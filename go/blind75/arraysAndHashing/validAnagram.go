5d95e8b98a7f3ac56165e744d5c4817f7e677a21
Eugene Moon <moonyoojin@gmail.com>
2023-03-12 20:52:34 -0400

    Add array solution

--- go/blind75/arraysAndHashing/validAnagram.go
+++ go/blind75/arraysAndHashing/validAnagram.go
@@ -1,32 +1,56 @@
 package arraysAndHashing
 
 // 217. Contains Duplicate (Easy)
+
+// hashmap solution
+// func IsAnagram(s string, t string) bool {
+// 	if len(s) != len(t) {
+// 		return false
+// 	}
+
+// 	s_map := map[rune]int{}
+// 	t_map := map[rune]int{}
+
+// 	for _, char := range s {
+// 		if _, ok := s_map[char]; !ok {
+// 			s_map[char] = 1
+// 		} else {
+// 			s_map[char] += 1
+// 		}
+// 	}
+
+// 	for _, char := range t {
+// 		if _, ok := t_map[char]; !ok {
+// 			t_map[char] = 1
+// 		} else {
+// 			t_map[char] += 1
+// 		}
+// 	}
+
+// 	for _, char := range s {
+// 		if s_map[char] != t_map[char] {
+// 			return false
+// 		}
+// 	}
+
+// 	return true
+// }
+
+// array solution
 func IsAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
 
-	s_map := map[rune]int{}
-	t_map := map[rune]int{}
+	var freq [26]int
 
-	for _, char := range s {
-		if _, ok := s_map[char]; !ok {
-			s_map[char] = 1
-		} else {
-			s_map[char] += 1
-		}
+	for idx := 0; idx < len(s); idx++ {
+		freq[s[idx]-'a']++
+		freq[t[idx]-'a']--
 	}
 
-	for _, char := range t {
-		if _, ok := t_map[char]; !ok {
-			t_map[char] = 1
-		} else {
-			t_map[char] += 1
-		}
-	}
-
-	for _, char := range s {
-		if s_map[char] != t_map[char] {
+	for idx := 0; idx < len(freq); idx++ {
+		if freq[idx] != 0 {
 			return false
 		}
 	}
