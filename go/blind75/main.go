595fca5120730521607e04fabe1de0410730120b
Eugene Moon <moonyoojin@gmail.com>
2023-03-12 20:57:31 -0400

    Fix problem number for valid anagram

--- go/blind75/main.go
+++ go/blind75/main.go
@@ -1,23 +1,17 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/yuzinu/leetcode/go/blind75/arraysAndHashing"
-)
-
 func main() {
 	// 217. Contains Duplicate (Easy)
-	// var test1 = arraysAndHashing.ContainsDuplicate([]int{1, 2, 3, 1})
-	// var test2 = arraysAndHashing.ContainsDuplicate([]int{1, 2, 3, 4})
-	// var test3 = arraysAndHashing.ContainsDuplicate([]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2})
-	// fmt.Println(test1)
-	// fmt.Println(test2)
-	// fmt.Println(test3)
+	// var containsDuplicateTest1 = arraysAndHashing.ContainsDuplicate([]int{1, 2, 3, 1})
+	// var containsDuplicateTest2 = arraysAndHashing.ContainsDuplicate([]int{1, 2, 3, 4})
+	// var containsDuplicateTest3 = arraysAndHashing.ContainsDuplicate([]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2})
+	// fmt.Println(containsDuplicateTest1)
+	// fmt.Println(containsDuplicateTest2)
+	// fmt.Println(containsDuplicateTest3)
 
-	// 217. Contains Duplicate (Easy)
-	var ia1 = arraysAndHashing.IsAnagram("anagram", "nagaram")
-	var ia2 = arraysAndHashing.IsAnagram("rat", "car")
-	fmt.Println(ia1)
-	fmt.Println(ia2)
+	// 242. Valid Anagram (Easy)
+	// var isAnagramTest1 = arraysAndHashing.IsAnagram("anagram", "nagaram")
+	// var isAnagramTest2 = arraysAndHashing.IsAnagram("rat", "car")
+	// fmt.Println(isAnagramTest1)
+	// fmt.Println(isAnagramTest2)
 }
