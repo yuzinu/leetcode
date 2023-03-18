3b1adab21734a5ffedd2cf2578151cb52783052d
Eugene Moon <moonyoojin@gmail.com>
2023-03-17 23:18:01 -0400

    Add valid anagram

---
+++ python/blind_75/arrays_and_hashing/valid_anagram.py
@@ -0,0 +1,25 @@
+from typing import List
+import unittest
+
+# 242. Valid Anagram (Easy)
+class Solution:
+  def is_anagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+      return False
+
+    countS, countT = {}, {}
+
+    for i in range(len(s)):
+      countS[s[i]] = 1 + countS.get(s[i], 0)
+      countT[t[i]] = 1 + countT.get(t[i], 0)
+    return countS == countT
+
+class TestSum(unittest.TestCase):
+  def test_1(self):
+    self.assertEqual(Solution.is_anagram(self, "anagram", "nagaram"), True, "Should be True")
+
+  def test_2(self):
+    self.assertEqual(Solution.is_anagram(self, "rat", "car"), False, "Should be False")
+
+if __name__ == '__main__':
+  unittest.main()
