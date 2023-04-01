3aa34029f0d325691e91b1fd52663b232d39fb18
Eugene Moon <moonyoojin@gmail.com>
2023-04-01 12:16:47 -0400

    Add product of array except self

---
+++ python/blind_75/arrays_and_hashing/product_of_array_except_self.py
@@ -0,0 +1,31 @@
+from typing import List
+import unittest
+
+# 347. Top K Frequent Elements (Medium)
+class Solution:
+  def product_except_self(self, nums: List[int]) -> List[int]:
+    res = [1] * (len(nums))
+
+    prefix = 1
+
+    for i in range(len(nums)):
+      res[i] = prefix
+      prefix *= nums[i]
+
+    postfix = 1
+
+    for i in range(len(nums) - 1, -1, -1):
+      res[i] *= postfix
+      postfix *= nums[i]
+
+    return res
+
+class Test(unittest.TestCase):
+  def test_1(self):
+    self.assertEqual(Solution.product_except_self(self, [1,2,3,4]), [24,12,8,6], "Should be [24,12,8,6]")
+
+  def test_2(self):
+    self.assertEqual(Solution.product_except_self(self, [-1,1,0,-3,3]), [0,0,9,0,0], "Should be [0,0,9,0,0]")
+
+if __name__ == '__main__':
+  unittest.main()
