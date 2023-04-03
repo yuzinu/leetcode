25a8f5266cc8fa742d78f9db3e3641393cd484cd
Eugene Moon <moonyoojin@gmail.com>
2023-04-03 13:57:13 -0400

    Add all missing golang tests

--- go/blind75/arraysAndHashing/arraysAndHashing_test.go
+++ go/blind75/arraysAndHashing/arraysAndHashing_test.go
@@ -1,8 +1,12 @@
 package arraysAndHashing
 
 import (
+	"fmt"
 	"reflect"
+	"sort"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // 217. Contains Duplicate (Easy)
@@ -41,6 +45,88 @@ func TestIsAnagram(t *testing.T) {
 	for i, test := range isAnagrams {
 		if output := IsAnagram(test.arg1, test.arg2); output != test.expected {
 			t.Errorf("Output %t not equal to expected %t for test %d", output, test.expected, i+1)
+		}
+	}
+}
+
+// 1. Two Sum (Easy)
+type twoSum struct {
+	arg1     []int
+	arg2     int
+	expected []int
+}
+
+var twoSums = []twoSum{
+	{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+	{[]int{3, 2, 4}, 6, []int{1, 2}},
+	{[]int{3, 3}, 6, []int{0, 1}},
+}
+
+func TestTwoSum(t *testing.T) {
+	for i, test := range twoSums {
+		if output := TwoSum(test.arg1, test.arg2); !reflect.DeepEqual(output, test.expected) {
+			t.Errorf("Output %q not equal to expected %q for test %d", output, test.expected, i+1)
+		}
+	}
+}
+
+// 49. Group Anagrams (Medium)
+type groupAnagram struct {
+	arg1     []string
+	expected [][]string
+}
+
+var groupAnagrams = []groupAnagram{
+	{[]string{"eat", "tea", "tan", "ate", "nat", "bat"}, [][]string{{"bat"}, {"nat", "tan"}, {"ate", "eat", "tea"}}},
+	{[]string{""}, [][]string{{""}}},
+	{[]string{"a"}, [][]string{{"a"}}},
+}
+
+func TestGroupAnagrams(t *testing.T) {
+	for i, test := range groupAnagrams {
+		t.Run(fmt.Sprintf("Group anagrams test %d", i+1), func(t *testing.T) {
+			output := GroupAnagrams(test.arg1)
+
+			// Sorting got inners
+			for _, inner := range output {
+				sort.Slice(inner, func(i, j int) bool {
+					return inner[i] < inner[j]
+				})
+			}
+
+			// Sorting want inners
+			for _, inner := range test.expected {
+				sort.Slice(inner, func(i, j int) bool {
+					return inner[i] < inner[j]
+				})
+			}
+
+			// Match
+			if ok := assert.ElementsMatch(t, output, test.expected); !ok {
+				t.Errorf("Output %q not equal to expected %q for test %d", output, test.expected, i+1)
+			}
+		})
+	}
+}
+
+// 347. Top K Frequent Elements (Medium)
+type topKFrequent struct {
+	arg1     []int
+	arg2     int
+	expected []int
+}
+
+var topKFrequents = []topKFrequent{
+	{[]int{1, 1, 1, 2, 2, 3}, 2, []int{1, 2}},
+	{[]int{1}, 1, []int{1}},
+	{[]int{200, 200, 0, 1, 1, 3, 3, 3, 200}, 3, []int{200, 3, 1}},
+}
+
+func TestTopKFrequent(t *testing.T) {
+	for i, test := range topKFrequents {
+		output := TopKFrequent(test.arg1, test.arg2)
+		if ok := assert.ElementsMatch(t, output, test.expected); !ok {
+			t.Errorf("Output %d not equal to expected %d for test %d", output, test.expected, i+1)
 		}
 	}
 }
--- go/blind75/main.go
+++ go/blind75/main.go
@@ -1,41 +1,4 @@
 package main
 
 func main() {
-	// 217. Contains Duplicate (Easy)
-	// var containsDuplicateTest1 = arraysAndHashing.ContainsDuplicate([]int{1, 2, 3, 1})
-	// var containsDuplicateTest2 = arraysAndHashing.ContainsDuplicate([]int{1, 2, 3, 4})
-	// var containsDuplicateTest3 = arraysAndHashing.ContainsDuplicate([]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2})
-	// fmt.Println(containsDuplicateTest1)
-	// fmt.Println(containsDuplicateTest2)
-	// fmt.Println(containsDuplicateTest3)
-
-	// 242. Valid Anagram (Easy)
-	// var isAnagramTest1 = arraysAndHashing.IsAnagram("anagram", "nagaram")
-	// var isAnagramTest2 = arraysAndHashing.IsAnagram("rat", "car")
-	// fmt.Println(isAnagramTest1)
-	// fmt.Println(isAnagramTest2)
-
-	// 1. Two Sum (Easy)
-	// var twoSumTest1 = arraysAndHashing.TwoSum([]int{2, 7, 11, 15}, 9)
-	// var twoSumTest2 = arraysAndHashing.TwoSum([]int{3, 2, 4}, 6)
-	// var twoSumTest3 = arraysAndHashing.TwoSum([]int{3, 3}, 6)
-	// fmt.Println(twoSumTest1)
-	// fmt.Println(twoSumTest2)
-	// fmt.Println(twoSumTest3)
-
-	// 49. Group Anagrams (Medium)
-	// var groupAnagramsTest1 = arraysAndHashing.GroupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
-	// var groupAnagramsTest2 = arraysAndHashing.GroupAnagrams([]string{""})
-	// var groupAnagramsTest3 = arraysAndHashing.GroupAnagrams([]string{"a"})
-	// fmt.Println(groupAnagramsTest1)
-	// fmt.Println(groupAnagramsTest2)
-	// fmt.Println(groupAnagramsTest3)
-
-	// 347. Top K Frequent Elements (Medium)
-	// var topKFrequentTest1 = arraysAndHashing.TopKFrequent([]int{1, 1, 1, 2, 2, 3}, 2)
-	// var topKFrequentTest2 = arraysAndHashing.TopKFrequent([]int{1}, 1)
-	// var topKFrequentTest3 = arraysAndHashing.TopKFrequent([]int{200, 200, 0, 1, 1, 3, 3, 3, 200}, 3)
-	// fmt.Println(topKFrequentTest1)
-	// fmt.Println(topKFrequentTest2)
-	// fmt.Println(topKFrequentTest3)
 }
